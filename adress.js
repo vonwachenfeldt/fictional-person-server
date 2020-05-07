98b2e63f20d86414fd9365dfb0a3a08630382789
David <davidvonwachenfeldt@gmail.com>
2020-05-07 15:27:38 +0200

    Added getAdress function. Improved methods.

--- adress.js
+++ adress.js
@@ -1,30 +1,28 @@
 function getNoun() {
-  return adresses.substantiv[(Math.random() * adresses.substantiv.length) | 0];
-  // |0=floor
+    return adresses.nouns[(Math.random() * adresses.substantiv.length) | 0];
+    // |0=floor
 }
 
 function getEnding() {
-  return adresses.ändelser[(Math.random() * adresses.ändelser.length) | 0];
+    return adresses.endings[(Math.random() * adresses.ändelser.length) | 0];
 }
 
 function getDirection() {
-  if ((Math.random() * 4) | (0 != 0)) {
-    return "";
-  }
-  return adresses.väderstreck[
-    (Math.random() * adresses.väderstreck.length) | 0
-  ];
+    if ((Math.random() * 4) | (0 != 0)) {
+        return "";
+    }
+    return adresses.directions[
+        (Math.random() * adresses.väderstreck.length) | 0
+    ];
 }
 
 function getNumber() {
-  return (Math.random() * 499 + 1) | 0;
+    return (Math.random() * 499 + 1) | 0;
 }
 
-for (var i = 0; i < 100; i++) {
-  var direction = getDirection();
-  const formattedDirection = direction == "" ? "" : direction + " "; // clean if statement
-  console.log(
-    `${formattedDirection}${getNoun() +
-      getEnding().toLowerCase()} ${getNumber()}`
-  );
-}
+module.exports = function getAdress() {
+    var direction = getDirection();
+    const formattedDirection = direction == "" ? "" : direction + " "; // clean if statement
+    var finalAdress =`${formattedDirection}${getNoun()+getEnding().toLowerCase()} ${getNumber()}`;
+    return finalAdress;
+};
