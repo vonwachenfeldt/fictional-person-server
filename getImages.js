03405caecc0f0c308234058ad40b513a546116c6
David <davidvonwachenfeldt@gmail.com>
2020-05-08 19:42:00 +0200

    Updated style.css. Age, hair color and eye color now available from getImage.js.

--- getImages.js
+++ getImages.js
@@ -16,7 +16,43 @@ module.exports.getImage = async function getImage(amount = 1, gender = ["female"
     const response = await fetch(URL, {
         headers: [["Authorization", "API-Key Cph30qkLrdJDkjW-THCeyA"]]
     });
-    return Promise.resolve(await response.json());
+
+    const JSON = await response.json();
+
+    for (let i = 0; i < JSON.images.length; i++) {
+        var meta = JSON.images[i].meta;
+    }
+
+    var age = meta.age.toString();
+    const hairColor = meta.hair_color.toString();
+    const eyeColor = meta.eye_color.toString();
+
+    switch (age) {
+        case "child": age = [7, 15];
+        case "young-adult": age = [16, 39];
+        case "adult": age = [40, 54];
+        case "elderly": age = [55, 80];
+        default: null;
+    }
+
+    switch (hairColor) {
+        case "gray": hairColor = "grå";
+        case "brown": hairColor = "brun";
+        case "blond": hairColor = "blond";
+        case "red": hairColor = "röd";
+        case "black": hairColor = "black";
+        default: null;
+    }
+
+    switch (eyeColor) {
+        case "gray": eyeColor = "grå";
+        case "brown": eyeColor = "brun";
+        case "blue": eyeColor = "blå";
+        case "green": eyeColor = "grön";
+        default: null;
+    }
+
+    return Promise.resolve(JSON);
 }
 
 module.exports.getImage(1).catch(err => console.log(err));
