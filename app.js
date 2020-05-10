02f249816942aac6848d5d0eb710d0d1e6ab731b
David <davidvonwachenfeldt@gmail.com>
2020-05-10 20:07:16 +0200

    fixed name of main.js to app.js

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const getPerson = require("./api/getPerson.js");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+
+app.get("/api/person", getPerson);
+
+module.exports = () => {
+    const module = {};
+
+    const port = process.env.app_port || process.env.PORT || 5000;
+
+    module.startServer = () => app.listen(port, () => console.log("fictional-person's server running on port ", port));
+
+    module.app = app;
+
+    return module;
+}
