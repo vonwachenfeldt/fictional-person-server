2307b9cb3ddbcc5b996d40b15267f6d6da53c591
Ludvig Magnusson <ludvig.magnusson04@gmail.com>
2021-02-21 14:14:08 +0100

    added a more random default seed

--- api/getPerson.js
+++ api/getPerson.js
@@ -1,8 +1,14 @@
 const Person = require("../api-utils/Person");
 
+const uuid = require("uuid").v4;
+
 async function getPerson(req, res) {
+    // Create a more random default seed
+    const defaultSeed = uuid();
+
     // Set the seed
-    var seed = parseInt(req.query.seed) || req.query.seed || Date.now();
+    // Prefer the seed as an int for backwards compatibility, otherwise use it as a string
+    var seed = parseInt(req.query.seed) || req.query.seed || defaultSeed;
     
     const person = new Person(seed);
     const generatedPerson = await person.generatePerson(req.query.gender, req.query.ageGroup);
