f84c16e48eb1d47e864d27ba892ca3fe6cb91add
Ludvig Magnusson <ludvig.magnusson04@gmail.com>
2021-02-21 15:21:49 +0100

    fixed string seed sometimes interpreted as an int

--- api/getPerson.js
+++ api/getPerson.js
@@ -2,13 +2,16 @@ const Person = require("../api-utils/Person");
 
 const uuid = require("uuid").v4;
 
+const isNumber = (num) => !isNaN(num);
+
 async function getPerson(req, res) {
     // Create a more random default seed
     const defaultSeed = uuid();
+    const qSeed = req.query.seed;
 
     // Set the seed
     // Prefer the seed as an int for backwards compatibility, otherwise use it as a string
-    var seed = parseInt(req.query.seed) || req.query.seed || defaultSeed;
+    var seed = isNumber(qSeed) ? parseInt(qSeed) : (req.query.seed || defaultSeed);
     
     const person = new Person(seed);
     const generatedPerson = await person.generatePerson(req.query.gender, req.query.ageGroup);
